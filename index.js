67f04bce1b0adb95ce7ee91893c5b6ab2aae233e
Lsong <song940@gmail.com>
2014-10-23 15:45:44 +0800

    redesign .

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-var fs 		= require('fs');
+var fs		= require('fs');
+var url		=	require('url');
 var path	= require('path');
+var mime 	= require('mime');
 
-module.exports = function(root){
+var Static = function(options){
+	this.options = options;
+};
+
+Static.prototype.send = function(req, res, next){
+	var uri = url.parse(req.url);
+	var pathname = uri.pathname;
+	if(/\/$/.test(pathname)) pathname += 'index.html';
+	var filename = path.join(this.options.root, pathname);
+	fs.stat(filename, function(err, stat){
+		if(err){
+			return next((~[ 'ENOENT' ].indexOf(err.code)) ? null : err);
+		}
+		var type = mime.lookup(filename);
+		var charset = mime.charsets.lookup(type);
+		res.setHeader('Content-Type'	, type + (charset ? '; charset=' + charset : '' ));
+		res.setHeader('Content-Length', stat.size);
+		res.setHeader('Last-Modified'	, stat.mtime.toUTCString());
+		
+		fs.createReadStream(filename).pipe(res);
+	});
+};
+
+module.exports = function(root, options){
+	options = options || {};
+	console.log(root);
+	options.root = root;
+	var static = new Static(options);	
 	return function(req, res, next){
-		var url = req.url;
-		if(/\/$/.test(url)) url += 'index.html';
-		var filename = path.join(root, url);
-		fs.exists(filename, function(exists){
-			if(exists){
-				fs.readFile(filename, { encoding: 'utf-8' }, function(err, data){
-					res.send(data);
-				});
-			}else{
-				next();
-			}
-		});
+		static.send.apply(static, arguments)
 	};
 };
--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
 var express	= require('express');
+var logger	= require('express-log');
 var static	= require('../index');
 
 var app = express();
 
+app.use(logger());
 app.use(static(__dirname + '/public'));
 
 var server = app.listen(5000, function(){
